bdba8fe3980b06c71a580ae698884053fc5ced88
SajeelHassan <sajeelh581@gmail.com>
2021-01-24 09:48:11 +0500

    Finished battleship project

---
+++ build/CMakeFiles/binary.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/binary.dir/src/bot.cpp.o"
+  "CMakeFiles/binary.dir/src/human.cpp.o"
+  "CMakeFiles/binary.dir/src/main.cpp.o"
+  "binary"
+  "binary.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/binary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
